1b11b938b6314f79dc5ca69459468e5e3dabc95b
David Flindall <hello@backtofront.london>
2020-01-22 15:06:13 +0000

    page title component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import PageTitle from "../components/PageTitle"
 
 const IndexPage = () => (
   <Layout>
+    
     <SEO title="Home" />
-    <h1>Hi people</h1>
+
+    <PageTitle title="We design brands and we build websites" />
+    
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
