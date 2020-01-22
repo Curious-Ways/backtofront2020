f994e4666fc2047cbc8624ba6cb30f1333f8cc59
David Flindall <hello@backtofront.london>
2020-01-22 16:27:57 +0000

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from 'styled-components'
+
+const Footer = styled.footer`
+
+`
+
+const Quote = props => (
+
+  <Footer>
+
+  </Footer>
+
+)
+
+export default Quote
